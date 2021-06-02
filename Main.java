cbe975806f9c43e588f8fc066a4be5a1e057a123
Evan D <Evan_delasota@yahoo.com>
2021-06-02 03:25:21 +0000

    Brute force solution, O(n^2) runtime

--- Main.java
+++ Main.java
@@ -6,10 +6,36 @@ class Main {
 
   }
 
-  public static int trap(int[] arr) {
-    int total = 0;
+  public static int trap(int[] height) {
+        if (height.length < 3) {
+            return 0;
+        }  
+        int total = 0;
+        
+        for (int i = 0; i < height.length; ++i) {
+            int maxLeft = 0;
+            int maxRight = 0;
+            int leftP = i;
+            int rightP = i;
+            
+            while (leftP >= 0) {
+                maxLeft = Math.max(height[leftP], maxLeft);
+                leftP--;
+            }
+            
+            while (rightP < height.length) {
+                maxRight = Math.max(height[rightP], maxRight);
+                rightP++;
+            }
+            
+            int currentWater = Math.min(maxLeft, maxRight) - height[i];
+            
+            if (currentWater >= 0) {
+                total += currentWater;
+            }
 
-
-    return total;
-  }
+        }
+        
+        return total;
+    }
 }
